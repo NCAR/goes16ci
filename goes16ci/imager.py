0a5132c8cbd9ae1b917872d7a80ba665cd2b7572
David John Gagne <djgagne@ou.edu>
2019-02-08 16:25:09 -0700

    Made GOES16ABI class.

--- goes16ci/imager.py
+++ goes16ci/imager.py
@@ -5,117 +5,168 @@ from datetime import datetime
 from pyproj import Proj, transform
 from os.path import join
 from scipy.interpolate import RectBivariateSpline
+import xarray as xr
 
 
-def abi_file_dates(files, file_date='c'):
+class GOES16ABI(object):
     """
-    Extract the file creation dates from a list of GOES-16 files.
-    Date format: Year (%Y), Day of Year (%j), Hour (%H), Minute (%M), Second (%s), Tenth of a second
-    See `AWS <https://docs.opendata.aws/noaa-goes16/cics-readme.html>`_ for more details
+    Handles data I/O and map projections for GOES-16 Advanced Baseline Imager data.
 
-    Args:
-        files (list): list of GOES-16 filenames.
-        file_date (str): Date in filename to extract. Valid options are
-            's' (start), 'e' (end), and 'c' (creation, default).
-    Returns:
-        :class:`pandas.DatetimeIndex`: Dates for each file
-    """
-    if file_date not in ['c', 's', 'e']:
-        file_date = 'c'
-    date_index = {"c": -1, "s": -3, "e": -2}
-    channel_dates = pd.DatetimeIndex(
-        [datetime.strptime(c_file[:-3].split("/")[-1].split("_")[date_index[file_date]][1:-1],
-                           "%Y%j%H%M%S") for c_file in files])
-    return channel_dates
-
-
-def goes16_abi_filename(date, channel, path, time_range_minutes=2):
-    """
-    Given a path to a dataset of GOES-16 files, find the netCDF file that matches the expected
-    date and channel, or band number.
-
-    The GOES-16 path should point to a directory containing a series of directories named by
-    valid date in %Y%m%d format. Each directory should contain Level 1 CONUS sector files.
-
-
-    Args:
-        date (:class:`str`, :class:`datetime.datetime`, or :class:`pandas.Timestamp`): contains the date of the image
-        channel (int): GOES-16 ABI `channel <https://www.goes-r.gov/mission/ABI-bands-quick-info.html>`_.
-        path (str): Path to top-level directory containing GOES-16 netCDF files.
-        time_range_minutes (int):
-    Returns:
-        str: full path to requested GOES-16 file
-    """
-    pd_date = pd.Timestamp(date)
-    channel_files = sorted(glob(join(path, pd_date.strftime("%Y%m%d"), f"OR_ABI-L1b-RadC-M3C{channel:02d}_G16_*.nc")))
-
-    channel_dates = abi_file_dates(channel_files)
-    file_index = np.where(np.abs(pd_date - channel_dates) < pd.Timedelta(minutes=time_range_minutes))[0]
-    if len(file_index) == 0:
-        raise FileNotFoundError('No GOES-16 files within 2 minutes of ' + pd_date)
-    else:
-        filename = channel_files[file_index[0]]
-    return filename
-
-
-def goes16_projection(goes16_ds):
-    """
-    Create a Pyproj projection object with the projection information from a GOES-16 file.
-    The geostationary map projection is described in the
-    `PROJ <https://proj4.org/operations/projections/geos.html>`_ documentation.
-
-    Args:
-        goes16_ds (:class:`xarray.Dataset`): GOES-16 dataset object containing projection information
-
-    Returns:
-        pyproj.Proj: Proj object with geostationary projection.
-    """
-    proj_dict = dict(proj="geos",
-                     h=goes16_ds["goes_imager_projection"].attrs["perspective_point_height"],
-                     lon_0=goes16_ds["goes_imager_projection"].attrs["longitude_of_projection_origin"],
-                     sweep=goes16_ds["goes_imager_projection"].attrs["sweep_angle_axis"])
-    return Proj(projparams=proj_dict)
-
-
-def sat_coordinates(goes16_ds):
-    """
-    Calculate the geostationary projection x and y coordinates in m for each
-    pixel in the image.
-
-    Args:
-        goes16_ds (:class:`xarray.Dataset`): GOES-16 image dataset
-
-    Returns:
-        :class:`numpy.ndarray`, :class:`numpy.ndarray` : x, y
+    Attributes:
+        date (:class:`pandas.Timestamp`): Date of image slices
+        channels (:class:`numpy.ndarray`): GOES-16 channels/bands to load
+        path (str): Path to top level of GOES-16 ABI directory.
+        time_range_minutes (int): interval in number of minutes to search for file that matches input time
+        goes16_ds (`dict` of :class:`xarray.Dataset` objects): Datasets for each channel
 
     """
-    sat_height = goes16_ds["goes_imager_projection"].attrs["perspective_point_height"]
-    x_h = goes16_ds["x"] * sat_height
-    y_h = goes16_ds["y"] * sat_height
-    x_g, y_g = np.meshgrid(x_h, y_h)
-    return x_g, y_g
+    def __init__(self, date, channels, path, time_range_minutes=2):
+        self.date = pd.Timestamp(date)
+        self.channels = np.array(channels, dtype=np.int32)
+        self.path = path
+        self.time_range_minutes = time_range_minutes
+        self.goes16_ds = dict()
+        self.channel_files = []
+        for channel in channels:
+            self.channel_files.append(self.goes16_abi_filename(channel))
+            self.goes16_ds[channel] = xr.open_dataset(self.channel_files[-1])
+        self.proj = self.goes16_projection()
+        self.x = None
+        self.y = None
+        self.x_g = None
+        self.y_g = None
+        self.lon = None
+        self.lat = None
+        self.sat_coordinates()
+        self.lon_lat_coords()
+
+    @staticmethod
+    def abi_file_dates(files, file_date='s'):
+        """
+        Extract the file creation dates from a list of GOES-16 files.
+        Date format: Year (%Y), Day of Year (%j), Hour (%H), Minute (%M), Second (%s), Tenth of a second
+        See `AWS <https://docs.opendata.aws/noaa-goes16/cics-readme.html>`_ for more details
+
+        Args:
+            files (list): list of GOES-16 filenames.
+            file_date (str): Date in filename to extract. Valid options are
+                's' (start), 'e' (end), and 'c' (creation, default).
+        Returns:
+            :class:`pandas.DatetimeIndex`: Dates for each file
+        """
+        if file_date not in ['c', 's', 'e']:
+            file_date = 'c'
+        date_index = {"c": -1, "s": -3, "e": -2}
+        channel_dates = pd.DatetimeIndex(
+            [datetime.strptime(c_file[:-3].split("/")[-1].split("_")[date_index[file_date]][1:-1],
+                               "%Y%j%H%M%S") for c_file in files])
+        return channel_dates
+
+    def goes16_abi_filename(self, channel):
+        """
+        Given a path to a dataset of GOES-16 files, find the netCDF file that matches the expected
+        date and channel, or band number.
+
+        The GOES-16 path should point to a directory containing a series of directories named by
+        valid date in %Y%m%d format. Each directory should contain Level 1 CONUS sector files.
 
 
-def lon_lat_coords(goes16_ds, projection):
-    """
-    Calculate longitude and latitude coordinates for each point in the GOES-16
-    image.
+        Args:
+            channel (int): GOES-16 ABI `channel <https://www.goes-r.gov/mission/ABI-bands-quick-info.html>`_.
+        Returns:
+            str: full path to requested GOES-16 file
+        """
+        pd_date = self.date
+        channel_files = sorted(glob(join(self.path, pd_date.strftime("%Y%m%d"),
+                                         f"OR_ABI-L1b-RadC-M3C{channel:02d}_G16_*.nc")))
+        channel_dates = self.abi_file_dates(channel_files)
+        file_index = np.where(np.abs(pd_date - channel_dates) < pd.Timedelta(minutes=self.time_range_minutes))[0]
+        if len(file_index) == 0:
+            raise FileNotFoundError('No GOES-16 files within 2 minutes of ' + pd_date)
+        else:
+            filename = channel_files[file_index[0]]
+        return filename
 
-    Args:
-        goes16_ds (:class:`xarray.Dataset`): GOES-16 image dataset
-        projection (pyproj.Proj): GOES-16 map projection
+    def goes16_projection(self):
+        """
+        Create a Pyproj projection object with the projection information from a GOES-16 file.
+        The geostationary map projection is described in the
+        `PROJ <https://proj4.org/operations/projections/geos.html>`_ documentation.
 
-    Returns:
-        :class:`numpy.ndarray`, :class:`numpy.ndarray` : longitudes, latitudes
-    """
-    x_g, y_g = sat_coordinates(goes16_ds)
-    lons, lats = projection(x_g, y_g, inverse=True)
-    lons[lons > 1e10] = np.nan
-    lats[lats > 1e10] = np.nan
-    return lons, lats
+        """
+        goes16_ds = self.goes16_ds[self.channels.min()]
+        proj_dict = dict(proj="geos",
+                         h=goes16_ds["goes_imager_projection"].attrs["perspective_point_height"],
+                         lon_0=goes16_ds["goes_imager_projection"].attrs["longitude_of_projection_origin"],
+                         sweep=goes16_ds["goes_imager_projection"].attrs["sweep_angle_axis"])
+        return Proj(projparams=proj_dict)
+
+    def sat_coordinates(self):
+        """
+        Calculate the geostationary projection x and y coordinates in m for each
+        pixel in the image.
+        """
+        goes16_ds = self.goes16_ds[self.channels.min()]
+        sat_height = goes16_ds["goes_imager_projection"].attrs["perspective_point_height"]
+        self.x = goes16_ds["x"].values * sat_height
+        self.y = goes16_ds["y"].values * sat_height
+        self.x_g, self.y_g = np.meshgrid(self.x, self.y)
+
+    def lon_lat_coords(self):
+        """
+        Calculate longitude and latitude coordinates for each point in the GOES-16
+        image.
+        """
+        self.lon, self.lat = self.proj(self.x_g, self.y_g, inverse=True)
+        self.lon[self.lon > 1e10] = np.nan
+        self.lat[self.lat > 1e10] = np.nan
+
+    def extract_image_patch(self, center_lon, center_lat, x_size_pixels, y_size_pixels):
+        """
+
+
+        Args:
+            center_lon:
+            center_lat:
+            x_size_pixels:
+            y_size_pixels:
+
+        Returns:
+
+        """
+        center_x, center_y = self.proj(center_lon, center_lat)
+        center_row = np.argmin(np.abs(self.y - center_y))
+        center_col = np.argmin(np.abs(self.x - center_x))
+        row_slice = slice(int(center_row - y_size_pixels // 2), int(center_row + y_size_pixels // 2))
+        col_slice = slice(int(center_col - x_size_pixels // 2), int(center_col + x_size_pixels // 2))
+        patch = np.zeros((1, y_size_pixels, x_size_pixels, self.channels.size), dtype=np.float32)
+        for c, channel in enumerate(self.channels):
+            patch[0, :, :, c] = self.goes16_ds[channel]["Rad"][row_slice, col_slice].values
+        return patch
+
+    def close(self):
+        for channel in self.channels:
+            self.goes16_ds[channel].close()
+            del self.goes16_ds[channel]
 
 
 def regrid_imagery(image, x_image, y_image, x_regrid, y_regrid, image_proj, regrid_proj, spline_kws=None):
+    """
+    For a given image, regrid it to another projection using spline interpolation.
+
+    Args:
+        image:
+        x_image:
+        y_image:
+        x_regrid:
+        y_regrid:
+        image_proj:
+        regrid_proj:
+        spline_kws:
+
+    Returns:
+
+    """
     if spline_kws is None:
         spline_kws = dict()
     x_regrid_image, y_regrid_image = transform(image_proj, regrid_proj, x_regrid.ravel(), y_regrid.ravel())
--- scripts/process_goes16.py
+++ scripts/process_goes16.py
@@ -1,9 +1,27 @@
 import numpy as np
 import pandas as pd
 from goes16ci.lightning import create_glm_grids
+from dask.distributed import LocalCluster, Client
+import argparse
+import yaml
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", required=True, help="Config yaml file")
+    parser.add_argument("-p", "--procs", type=int, default=1, help="Number of processes")
+    parser.add_argument("-l", "--glm", action="store_true", help="Create GLM grids")
+    parser.add_argument("-p", "--patch", action="store_true", help="Sample GOES-16 patches")
+    args = parser.parse_args()
+    with open(args.config, "r") as config_file:
+        config = yaml.load(config_file)
+    cluster = LocalCluster(n_workers=args.procs)
+    client = Client(cluster)
+
+    return
+
+
+def glm_single_date():
     glm_path = "/Users/dgagne/data/goes16_nc/GLM-L2_LCFA"
     out_path = '/Users/dgagne/data/goes16_glm_grids'
     start_date = pd.Timestamp("2018-05-01 03:00:00")
