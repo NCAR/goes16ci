c11355d5a4867542089c47878ed4c6234a1dda2c
djgagne@ou.edu <djgagne@ou.edu>
2019-03-25 10:28:48 -0600

    Added casper submission script

---
+++ goes16_benchmark_casper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --job-name=goes16ci
+#SBATCH --account=NAML0001
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=1
+#SBATCH --time=01:00:00
+#SBATCH --partition=dav
+#SBATCH --gres=gpu:v100:4
+#SBATCH --exclusive
+#SBATCH --mem=128G
+module purge
+module load gnu/7.3.0 openmpi-x/3.1.0 python/3.6.4 cuda
+source /glade/work/dgagne/ncar_pylib_dl/bin/activate
+cd ~/goes16ci
+python -u goes16_deep_learning_benchmark.py >& goes_deep.log
