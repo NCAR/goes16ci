9b9ff0bedfa5afc4967fce30a4cf85948e864c00
guwa4340 <guwa4340@colorado.edu>
2020-01-06 14:37:40 -0700

    Changes made to produce different benchmarks

--- goes16_benchmark_casper.sh
+++ goes16_benchmark_casper.sh
@@ -10,7 +10,9 @@
 #SBATCH --mem=256G
 module purge
 module load gnu/7.3.0 openmpi python/3.6.8 cuda/10.0
-source /glade/work/dgagne/ncar_pylib_dl_10/bin/activate
+export PATH="/glade/u/home/gwallach/.conda/envs/goes16/bin:$PATH"
 cd ~/goes16ci
 python setup.py install
-python -u goes16_deep_learning_benchmark.py >& goes_deep.log
+python -u goes16_deep_learning_benchmark_32.py >& goes_deep.log
+python -u goes16_deep_learning_benchmark_64.py >& goes_deep.log
+python -u goes16_deep_learning_benchmark_128.py >& goes_deep.log
