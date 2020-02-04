6215922eae4100d0d881761b1ed7dd1bd74f6375
guwa4340 <guwa4340@colorado.edu>
2020-02-04 12:01:38 -0700

    .gitignore added

--- goes16_benchmark_casper.sh
+++ goes16_benchmark_casper.sh
@@ -9,10 +9,11 @@
 #SBATCH --exclusive
 #SBATCH --mem=256G
 module purge
-#module load gnu/7.3.0 openmpi python/3.6.8 cuda/10.0 #changed to python 3.6 instead of 3.7 01/16 because tf2.0 is only compatable with python 3.6
-#export PATH="/glade/u/home/gwallach/.conda/envs/goes16/bin:$PATH"
+module load gnu/7.3.0 openmpi python/3.6.8 cuda/10.0 #changed to python 3.6 instead of 3.7 01/16 because tf2.0 is only compatable with python 3.6
+export PATH="/glade/u/home/gwallach/.conda/envs/goes16/bin:$PATH"
 module load ncarenv
 ncar_pylib
+python setup.py install
 python -u goes16_deep_learning_benchmark_32.py >& goes_deep32.log
 python -u goes16_deep_learning_benchmark_64.py >& goes_deep64.log
 #python -u goes16_deep_learning_benchmark_128.py >& goes_deep.log
