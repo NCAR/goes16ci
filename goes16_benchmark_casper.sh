1f118898b0a9abefb1b1beeda09e9af0c004bc14
djgagne@ou.edu <djgagne@ou.edu>
2020-03-31 19:50:37 -0600

    Updated casper submission script

--- goes16_benchmark_casper.sh
+++ goes16_benchmark_casper.sh
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu:v100:8
 #SBATCH --exclusive
 #SBATCH --mem=768G
-module load ncarenv/1.3 gnu/7.4.0 ncarcompilers/0.5.0 python/3.7.5 cuda/10.0
+module load ncarenv/1.3 gnu/7.4.0 ncarcompilers/0.5.0 python/3.7.5 cuda/10.1
 ncar_pylib ncar_20191211 
 pip install -e .
 python -u goes16_deep_learning_benchmark.py -c benchmark_config_default.yml >& goes_deep_default.log
