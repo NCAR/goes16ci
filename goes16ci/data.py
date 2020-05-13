7483aa651f45fda16f0c56f8216f70f3140e98bb
guwa4340 <guwa4340@colorado.edu>
2020-05-13 11:41:12 -0600

    splitting data function added

--- goes16ci/data.py
+++ goes16ci/data.py
@@ -74,3 +74,25 @@ def load_data_parallel(data_path, num_processes,
     del cluster
     return all_images, all_counts, all_time
 
+def split_data(train_start, train_end, val_start, val_end, test_start, test_end, all_data, all_counts, all_time):
+    train_indices = np.where((all_time >= train_start) & (all_time <=train_end))[0])
+    val_indices  = np.where((all_time >= val_start) & (all_time <=val_end))[0] 
+    test_indices = np.where((all_time >= test_start) & (all_time <=test_end))[0]
+    train_data = all_data[train_indices].astype("float32")
+    val_data = all_data[val_indices].astype("float32")
+    test_data = all_data[test_indices].astype("float32")
+    train_counts = np.where(all_counts[train_indices] > 0, 1, 0).astype('float32')
+    val_counts = np.where(all_counts[val_indices] > 0, 1, 0).astype('float32')
+    test_counts = np.where(all_counts[test_indices] > 0, 1, 0).astype('float32')
+    data_subsets = {}
+    counts_subsets = {}
+    data_subsets[“train”] = train_data
+    data_subsets[“val”] = val_data
+    data_subsets[“test”] = test_data
+    counts_subsets[“train”] = train_counts
+    counts_subsets[“val”] = val_counts
+    counts_subsets[“test”] = test_counts
+    return data_subsets, counts_subsets
+    
+    
+    
