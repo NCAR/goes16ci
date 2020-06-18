752e189b7ecd2e57ba97c79b1afa0d740d6b922b
djgagne@ou.edu <djgagne@ou.edu>
2020-06-17 18:37:49 -0600

    Updated config scripts

--- goes16_benchmark_casper_res.sh
+++ goes16_benchmark_casper_res.sh
@@ -3,13 +3,15 @@
 #SBATCH --account=NAML0001
 #SBATCH --ntasks=36
 #SBATCH --cpus-per-task=1
-#SBATCH --time=01:00:00
+#SBATCH --time=00:30:00
 #SBATCH --partition=dav
 #SBATCH --gres=gpu:v100:8
 #SBATCH --exclusive
 #SBATCH --reservation=casper_8xV100
 #SBATCH --mem=768G
-module load ncarenv/1.3 gnu/7.4.0 ncarcompilers/0.5.0 python/3.7.5 cuda/10.0
-ncar_pylib ncar_20191211 
+#module load ncarenv/1.3 gnu/7.4.0 ncarcompilers/0.5.0 python/3.7.5 cuda/10.0
+#ncar_pylib ncar_20191211 
+module load cuda/10.1
+export PATH="/glade/u/home/dgagne/miniconda3/envs/goes/bin/:$PATH"
 pip install -e .
 python -u goes16_deep_learning_benchmark.py -c benchmark_config_default.yml >& goes_deep_default.log
