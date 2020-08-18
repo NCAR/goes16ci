f6d9368e88ab37d46c5f6df9ed06b89835d03617
Bill Petzke <bpetzke@ucar.edu>
2020-08-18 14:00:23 -0600

    Modifications to patch_inference.py to make output dimensions match GLM gridded nc files

--- scripts/patch_inference.py
+++ scripts/patch_inference.py
@@ -10,14 +10,12 @@ from tensorflow.keras.models import load_model
 Script to perform inference of a patchfile using a trained Tensorflow NN model
 
 Example Usage:
-#Random patches
-./patch_inference.py "/glade/scratch/gwallach/goes16/goes16ci_model_cpu2020-06-10 16:14:15.932711.h5" "/glade/scratch/gwallach/goes16_nc/ABI_patches_32/abi_patches_20190915T000000.nc" lightning_20190915T000000_out.nc
 #Midwest
-./patch_inference.py "/glade/scratch/gwallach/goes16/goes16ci_model_cpu2020-06-10 16:14:15.932711.h5" "/glade/p/ral/wsap/petzke/lightning/abi/patches/abi_patches_20190601T180000.nc" lightning_20190601T18000_out.nc
+./patch_inference.py -v "/glade/scratch/gwallach/goes16/goes16ci_model_cpu2020-06-10 16:14:15.932711.h5" /glade/p/ral/wsap/petzke/lightning/abi/patches/abi_patches_20190601T180000.nc /glade/p/ral/wsap/petzke/lightning/glm/grid/glm_grid_s20190601T180000_e20190601T230000.nc lightning_20190601T18000_out.nc
 """
 
 
-def infer(modelfile,patchfile,outfile,verbose=False):
+def infer(modelfile,patchfile,glmtemplate,outfile,verbose=False):
     model = load_model(modelfile)
     if verbose:
         model.summary()
@@ -28,11 +26,10 @@ def infer(modelfile,patchfile,outfile,verbose=False):
     time_data = nc['time'][:]
     lon_data = nc['lon'][:]
     lat_data = nc['lat'][:]
-    patch_data = nc['patch'][:]
 
     patch_dim_size = nc.dimensions['patch'].size
-    y_dim_size = nc.dimensions['y'].size
-    x_dim_size = nc.dimensions['x'].size    
+    patch_y_dim_size = nc.dimensions['y'].size
+    patch_x_dim_size = nc.dimensions['x'].size    
     nc.close()
 
     abi_data = np.swapaxes(abi_data,1,3)
@@ -43,37 +40,43 @@ def infer(modelfile,patchfile,outfile,verbose=False):
     print("Performing inference",flush=True)
     y_hat = model.predict(abi_data)
 
-    y_hat = y_hat.reshape((len(y_hat),))
+    print("Reading output template netcdf")
+    nc = netCDF4.Dataset(glmtemplate)
+    y_dim_size = nc.dimensions['y'].size
+    x_dim_size = nc.dimensions['x'].size
+    time_dim_size = nc.dimensions['time'].size
+    nc.close()
+
+    y_hat = y_hat.reshape((time_dim_size,y_dim_size,x_dim_size))
     if verbose:
         print("y_hat.shape = %s, y_hat = %s" % (y_hat.shape,y_hat),flush=True)
 
     print("Creating output file")
     ncout = netCDF4.Dataset(outfile, 'w', format='NETCDF4')
 
-    ncout.createDimension('patch', patch_dim_size)
+    ncout.createDimension('y', y_dim_size)
+    ncout.createDimension('x', x_dim_size)
+    ncout.createDimension('time', time_dim_size)
 
-    prob_light = ncout.createVariable('prob_lightning', np.dtype(np.float32).char, ('patch',))
-    prob_light.fill_value = np.nan
-    prob_light[:] = y_hat
+    mid_y = patch_y_dim_size//2
+    mid_x = patch_x_dim_size//2
 
-    time_var = ncout.createVariable('time', np.dtype(np.int64).char, ('patch',))
+    lon_var = ncout.createVariable('lon', np.float64, ('y','x',))
+    lon_var.fill_value = np.nan
+    lon_var[:] = lon_data[:patch_dim_size//time_dim_size,mid_y,mid_x].reshape((y_dim_size,x_dim_size))
+
+    lat_var = ncout.createVariable('lat', np.float64, ('y','x',))
+    lat_var.fill_value = np.nan
+    lat_var[:] = lat_data[:patch_dim_size//time_dim_size,mid_y,mid_x].reshape((y_dim_size,x_dim_size))
+
+    time_var = ncout.createVariable('time', np.int64, ('time',))
     time_var.units = 'minutes since 2019-06-01 18:20:00'
     time_var.calendar = 'proleptic_gregorian'
-    time_var[:] = time_data
+    time_var[:] = np.unique(time_data)
 
-    mid_y = y_dim_size//2
-    mid_x = x_dim_size//2
-
-    lon_var = ncout.createVariable('lon', np.dtype(np.float32).char, ('patch',))
-    lon_var.fill_value = np.nan
-    lon_var[:] = lon_data[:,mid_y,mid_x]
-
-    lat_var = ncout.createVariable('lat', np.dtype(np.float32).char, ('patch',))
-    lat_var.fill_value = np.nan
-    lat_var[:] = lat_data[:,mid_y,mid_x]
-
-    patch_var = ncout.createVariable('patch', np.dtype(np.int64).char, ('patch',))
-    patch_var[:] = patch_data
+    light_prob = ncout.createVariable('lightning_prob', np.float64, ('time','y','x',))
+    light_prob.fill_value = np.nan
+    light_prob[:] = y_hat
 
     ncout.close()
 
@@ -82,10 +85,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("modelfile",type=str,help="HDF5 model file")
     parser.add_argument("patchfile",type=str,help="Input nc patch file")
+    parser.add_argument("glmtemplate",type=str,help="Output GLM template. Should use the same dimensions used to create patch file.")
     parser.add_argument("outfile",type=str,help="Output nc file")
     parser.add_argument("-v","--verbose",dest="verbose",help="Provide extra debugging information",action="store_true")
     args = parser.parse_args()
-    infer(args.modelfile,args.patchfile,args.outfile,args.verbose)
+    infer(args.modelfile,args.patchfile,args.glmtemplate,args.outfile,args.verbose)
 
 
 if __name__ == '__main__':
