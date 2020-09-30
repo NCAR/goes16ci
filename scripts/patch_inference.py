Fixed Patch Inference For Scaling

scripts/patch_inference.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import argparse
-
+import pandas as pd
 import netCDF4
 import numpy as np
 from tensorflow.keras.models import load_model
+from goes16ci.models import MinMaxScaler2D
 
 """
 Script to perform inference of a patchfile using a trained Tensorflow NN model
@@ -34,11 +35,32 @@ def infer(modelfile,patchfile,glmtemplate,outfile,verbose=False):
 
     abi_data = np.swapaxes(abi_data,1,3)
     abi_data = np.swapaxes(abi_data,1,2)
+    print("Not Scaled = ",abi_data.shape)
+    #rescale ABI data to pass into Model
+    scaler = MinMaxScaler2D()
+    scaler.scale_values = pd.read_csv("../scaler_values.csv")
+    abi_data_scaled = 1.0 - scaler.transform(abi_data)
+    print("Scaled = ",abi_data_scaled.shape)
+    
+    """
+    scaling values need to be saved out during training, then the patch builder and patch inference
+    functions need to load the scaling values and apply them to the patches in order for the neural network
+    to produce reasonable predictions. 
+    After values are MinMaxScaled, then the scaled values need to be subracted from 1
+    
+    Code used to do this in the benchmark script:
+    # Rescale training and validation data
+    scaler = MinMaxScaler2D()
+    train_data_scaled = 1.0 - scaler.fit_transform(train_data)
+    val_data_scaled = 1.0 - scaler.transform(val_data)
+    print(scaler.scale_values)
+    
+    """
     if verbose:
         print("abi_data.shape = %s, abi_data = %s" % (abi_data.shape,abi_data[[0,-1]]),flush=True)
 
     print("Performing inference",flush=True)
-    y_hat = model.predict(abi_data)
+    y_hat = model.predict(abi_data_scaled)
 
     print("Reading output template netcdf")
     nc = netCDF4.Dataset(glmtemplate)