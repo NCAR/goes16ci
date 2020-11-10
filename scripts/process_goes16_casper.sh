af131ac87edfeaaa099d60cd94f25cf62ec236c4
Gunther Wallach <gwallach98@gmail.com>
2020-11-10 12:30:42 -0700

    Update process_goes16_casper.sh

--- scripts/process_goes16_casper.sh
+++ scripts/process_goes16_casper.sh
@@ -8,18 +8,6 @@
 #SBATCH -e goes16_process.log
 #SBATCH -o goes16_process.log
 #SBATCH --mail-type=BEGIN,END,FAIL
-<<<<<<< HEAD
-#SBATCH --mail-user=gwallach
-export PATH="/glade/u/home/gwallach/.conda/envs/goes16/bin:$PATH"
-cd $HOME/goes16ci/
-pip install .
-cd scripts
-python -u process_goes16.py goes16_process_config_32.yml -n 6 -l >& process.log
-python -u process_goes16.py goes16_process_config_64.yml -n 6 -l >& process.log
-python -u process_goes16.py goes16_process_config_32.yml -n 6 -a >& process.log
-python -u process_goes16.py goes16_process_config_64.yml -n 6 -a >& process.log
-#python -u process_goes16.py goes16_process_config_128.yml -n 36 -a >& process.log
-=======
 #SBATCH --mail-user=dgagne
 module purge
 export HOME="/glade/u/home/gwallach"
@@ -29,4 +17,3 @@ cd $HOME/goes16ci
 python setup.py install
 cd $HOME/goes16ci/scripts
 python -u process_goes16.py goes16_process_config.yml -n 36 -a 
->>>>>>> 8e3b2e64068e40c4a4b42a8c38663ad4a8b4ce43
