2caf2f4d02f7395c922862cec84ef740642866e7
guwa4340 <guwa4340@colorado.edu>
2021-02-01 15:03:30 -0700

    Updated Casper Benchmark Scripts for CUDA

--- goes16_benchmark_casper_res.sh
+++ goes16_benchmark_casper_res.sh
@@ -10,4 +10,5 @@
 #SBATCH --reservation=casper_8xV100
 #SBATCH --mem=768G
 export PATH=/glade/u/home/gwallach/.conda/envs/goes/bin:$PATH
+module load cuda/11 cudnn nccl
 python -u goes16_deep_learning_benchmark.py -c benchmark_config_default-Gunther.yml >& goes_deep_default.log
