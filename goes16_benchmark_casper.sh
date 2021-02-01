cecbe4f523d480fc915aec08e6e72bd01f18fe13
guwa4340 <guwa4340@colorado.edu>
2021-02-01 14:57:22 -0700

    Ready for Pull Request

--- goes16_benchmark_casper.sh
+++ goes16_benchmark_casper.sh
@@ -8,5 +8,5 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:v100:1
 export PATH=/glade/u/home/gwallach/.conda/envs/goes/bin:$PATH
-
+module load cuda/11 cudnn nccl
 python -u goes16_deep_learning_benchmark.py -c benchmark_config_default-Gunther.yml >& goes_deep.log
